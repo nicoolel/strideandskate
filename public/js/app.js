a9f323c4622d375971415ca6e6a53962842f3aac
nicoolel <nicolees1123@gmail.com>
2021-10-17 21:49:23 -0700

    styled landing page

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+// document.getElementsByClassName("navbar-toggler").addEventListener("click", changeBackground)
+
+// function changeBackground(){
+//    document.querySelecto('#mainNavBar').classList.toggle("activated")
+//    console.log(error)
+// }
+
+// function myFunction() {
+//     var element = document.getElementById("myDIV");
+//     element.classList.toggle("mystyle");
+//   }
