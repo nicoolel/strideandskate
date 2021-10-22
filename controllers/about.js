c0a1a66a7e8e0c1b72c8fdbe815c3e69a2df466e
nicoolel <nicolees1123@gmail.com>
2021-10-22 14:41:58 -0700

    set up about page and route

---
+++ controllers/about.js
@@ -0,0 +1,6 @@
+module.exports = {
+    getAbout: (req, res) => {
+      res.render("about.ejs");
+    },
+  };
+  
--- routes/main.js
+++ routes/main.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
 const postsController = require("../controllers/posts");
+const aboutController = require("../controllers/about");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/about", aboutController.getAbout);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
