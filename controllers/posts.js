22202e34d43bd5682e0c808833c2d6f3b2546900
nicoolel <nicolees1123@gmail.com>
2021-11-03 18:32:15 -0700

    separated users posts and add posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,6 +11,14 @@ module.exports = {
       console.log(err);
     }
   },
+  getMyPosts: async (req, res) => {
+    try {
+      const posts = await Post.find({ user: req.user.id });
+      res.render("myPosts.ejs", { posts: posts, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
   getFeed: async (req, res) => {
     try {
       // find posts and sort from the most recent to the least recent
--- routes/main.js
+++ routes/main.js
@@ -9,6 +9,7 @@ const { ensureAuth, ensureGuest } = require("../middleware/auth");
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/myPosts", ensureAuth, postsController.getMyPosts);
 router.get("/about", aboutController.getAbout);
 router.get("/feed", ensureAuth, postsController.getFeed);
 router.get("/login", authController.getLogin);
